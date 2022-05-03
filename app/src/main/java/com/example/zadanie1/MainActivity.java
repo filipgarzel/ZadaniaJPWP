1ab88cf5d2776da8a81a851aaa336a70e18d6b65
byczu123 <byczek@student.agh.edu.pl>
2022-05-03 21:23:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/zadanie1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.zadanie1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+//Tutaj należy skorzystać z metody onClick która znajduje się w pliku xml
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/example/zadanie1/MainActivity2.java
@@ -0,0 +1,20 @@
+package com.example.zadanie1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        //Tutaj wszystkie metody należy ustawić w kodzie, za pomocą OnClickListener'ów
+    }
+}
